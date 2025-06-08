aab148aec053a349b1347deb09173483c2aa489d
mcarbono3 <ing.mariocarbono@gmail.com>
2025-06-08 17:51:58 -0500

    chat.js

--- netlify/functions/chat.js
+++ netlify/functions/chat.js
@@ -4,7 +4,7 @@ const { processQuery, determineQueryType, processSpecificTask } = require('./uti
 exports.handler = async (event, context) => {
   // Configurar CORS
   const headers = {
-    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Origin': 'https://mcarbono3.github.io',
     'Access-Control-Allow-Headers': 'Content-Type, Authorization',
     'Access-Control-Allow-Methods': 'POST, OPTIONS',
     'Content-Type': 'application/json'
